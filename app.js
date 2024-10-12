9b30e0e781aceef39d0070838ece8698ad1f1c57
Stefan Milic <stefanmilic189@gmail.com>
2024-10-12 20:35:28 +0200

    add title on hp

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5555
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('1.1') // change this string to ensure a new version deployed
+  res.send('V1.2') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
